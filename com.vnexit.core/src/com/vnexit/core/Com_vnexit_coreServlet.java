228f51a45b5900a65a4c7518c7c6222349cf2abc
leviet <leviet@vnexit.com>
2014-04-09 20:06:57 +0700

    Init commit  On branch master 	new file:   com.vnexit.core/.classpath 	new file:   com.vnexit.core/.project 	new file:   com.vnexit.core/.settings/com.google.appengine.eclipse.core.prefs 	new file:   com.vnexit.core/.settings/com.google.gdt.eclipse.core.prefs 	new file:   com.vnexit.core/.settings/org.eclipse.jdt.core.prefs 	new file:   com.vnexit.core/.settings/org.eclipse.wst.common.project.facet.core.xml 	new file:   com.vnexit.core/src/META-INF/jdoconfig.xml 	new file:   com.vnexit.core/src/META-INF/persistence.xml 	new file:   com.vnexit.core/src/com/vnexit/core/Com_vnexit_coreServlet.java 	new file:   com.vnexit.core/src/log4j.properties 	new file:   com.vnexit.core/war/WEB-INF/.gitignore 	new file:   com.vnexit.core/war/WEB-INF/appengine-web.xml 	new file:   com.vnexit.core/war/WEB-INF/logging.properties 	new file:   com.vnexit.core/war/WEB-INF/web.xml 	new file:   com.vnexit.core/war/favicon.ico 	new file:   com.vnexit.core/war/index.html

---
+++ com.vnexit.core/src/com/vnexit/core/Com_vnexit_coreServlet.java
@@ -0,0 +1,11 @@
+package com.vnexit.core;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Com_vnexit_coreServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
